5798a39a8e1668780d46ad52128f2d574ac75a14
Andreas Hahn <a.hahn@algomedia.de>
2014-09-19 08:29:35 +0200

    Initial commit

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+
+$plugins = array();
+
+$plugins["1"] = new PluginDefinition("1", "codemirror.tid", "CodeMirror Core Plugin", "http://www.tiddlywiki.com/codemirrordemo.html", "editor");  
+
+
+?>
