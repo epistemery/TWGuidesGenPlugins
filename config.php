cef0bef61f9b76e99f99bb1abbc34a1cbbfe3549
Andreas Hahn <www.gallus@googlemail.com>
2014-09-19 15:36:52 +0200

    introduced more reasonable categories

--- config.php
+++ config.php
@@ -5,19 +5,19 @@
 // new PluginDefinition("#"; "filename", "Display Name", "Website URI", "category" )
 //
 // categories currently include
-// * editor => Editor plugins
-// * parser => Parser plugins
-// * third	=> Thrid party library plugins
+// * core => Core Plugins
+// * widgets => Widgets
+// * macros	=> Macros
 //
 // Feel free to add categories
 
 $plugins = array();
 
-$plugins["1"] = new PluginDefinition("1", "codemirror.tid", "CodeMirror Core Plugin", "http://www.tiddlywiki.com/codemirrordemo.html", "editor");  
-$plugins["2"] = new PluginDefinition("2", "markdown.tid", "Markdown Core Plugin", "http://www.tiddlywiki.com/markdowndemo.html", "parser");  
-$plugins["3"] = new PluginDefinition("3", "tw2parser.tid", "Classic Parser Core Plugin", "http://www.tiddlywiki.com/classicparserdemo.html", "parser");  
-$plugins["4"] = new PluginDefinition("4", "highlight.tid", "Highlight.js Core Plugin", "http://www.tiddlywiki.com/highlightdemo.html", "third");  
-$plugins["5"] = new PluginDefinition("5", "d3.tid", "D3.js Core Plugin", "http://www.tiddlywiki.com/d3demo.html", "third");  
+$plugins["1"] = new PluginDefinition("1", "codemirror.tid", "CodeMirror Core Plugin", "http://www.tiddlywiki.com/codemirrordemo.html", "core");  
+$plugins["2"] = new PluginDefinition("2", "markdown.tid", "Markdown Core Plugin", "http://www.tiddlywiki.com/markdowndemo.html", "core");  
+$plugins["3"] = new PluginDefinition("3", "tw2parser.tid", "Classic Parser Core Plugin", "http://www.tiddlywiki.com/classicparserdemo.html", "core");  
+$plugins["4"] = new PluginDefinition("4", "highlight.tid", "Highlight.js Core Plugin", "http://www.tiddlywiki.com/highlightdemo.html", "core");  
+$plugins["5"] = new PluginDefinition("5", "d3.tid", "D3.js Core Plugin", "http://www.tiddlywiki.com/d3demo.html", "core");  
 
 
 ?>
